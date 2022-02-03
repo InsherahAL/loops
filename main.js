86f1950d863c04d2a9a6572346bfbb86363a56f1
InsherahAL <juju.abulibdeh@gmail.com>
2022-02-03 16:47:57 +0200

    loops

---
+++ main.js
@@ -0,0 +1,19 @@
+// let count = 1
+// let i=0
+// while (count<=1000) {
+//     console.log(count);
+//    //count +=3
+//    //count*=2
+//    count*=2
+//    i++
+// }
+// let msg = 'finished counting in '
+// msg+=i + ' iterations '
+// console.log(msg);
+let star = '*'
+let i =1
+while (i<=10) {
+    console.log(star);
+    star +='*'
+    i++
+}
