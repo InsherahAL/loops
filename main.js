af80c3a50299a5459daa45f61c89023f1c3797ae
InsherahAL <juju.abulibdeh@gmail.com>
2022-02-08 17:32:25 +0200

    changes

--- main.js
+++ main.js
@@ -10,10 +10,55 @@
 // let msg = 'finished counting in '
 // msg+=i + ' iterations '
 // console.log(msg);
-let star = '*'
-let i =1
-while (i<=10) {
-    console.log(star);
-    star +='*'
-    i++
-}
+// let star = '*'
+// let i =1
+// while (i<=10) {
+//     console.log(star);
+//     star +='*'
+//     i++
+// }
+// const str ='hfbvhgfglfbdhbvshv hvhgfeg hdfgsyfgekgF'
+// for (let i = 0; i < str.length ; i++) {
+//    console.log(str[i]);
+    
+// }
+// const str = 'ghafgvufgaue ygvaduhbfyaeug'
+// for (let i = 0; str[i] !=undefined ; i++) {
+//    console.log(str[i]);
+    
+// }
+//  const str = 'ghafgvufgaue ygvaduhbfyaeug'
+// for (let i = 0; str[i]; i++) {
+//    console.log(str[i]);
+    
+// }
+// if (str[20]) {
+//     console.log('truthy');
+// } else {
+//     console.log('falsy');
+// }
+// undefined//falsy
+// null//falsy
+// NaN//falsy
+// 0//falsy
+// const str = 'ghafgvFGue ygvJLhb fyaeug fLLhbdz'
+// let count = 0   
+// str[6] == str[6].toUpperCase
+// for (let i = 0; i < str.length; i++) {
+
+
+// if (str[i] == ' ') {
+//     count++
+// }    
+// }                
+//  console.log(count);
+// const str = 'ghafgvFGue ygvJLhb fyaeug fLLhbdz'
+// let count = 0
+
+// for (let i = 0; i < str.length; i++) {
+//   if (str.charCodeAt(i)>=65 && str.charCodeAt(i) <=90) {
+//      count++
+//   }
+    
+// }
+// console.log(count);
